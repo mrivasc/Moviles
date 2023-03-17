d70d9ed6987db6bb53c5ce7621a12be2571d25d3
Michelle Rivas <00015121@uca.edu.sv>
2023-03-17 02:24:40 -0600

    adding new project

---
+++ ContactPage/app/src/main/java/com/mrivas/contactpage/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.mrivas.contactpage
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var contactButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bind()
+        addListener()
+    }
+
+    private fun bind() {
+        contactButton = findViewById(R.id.contact_button)
+    }
+
+    private fun addListener() {
+        contactButton.setOnClickListener {
+            var intent = Intent(MainActivity@this, ResultActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
